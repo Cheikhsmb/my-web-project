4d80f604b1826e997f390d7b7f65624c0d68cc18
Cheikh SAMB <sambkhadim031@gmail.com>
2025-03-04 11:50:54 +0000

    Updated app.js with new content

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello, Git !")
+console.log("Hello, Git !");
+  let ph = 9
+  
+  if (ph > 7){
+    console.log("Basic")
+  } else if (ph < 7 ){
+    console.log("Acidic")
+  }else {
+    console.log("Neutral")
+  }
