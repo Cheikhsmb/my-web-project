071bcfb8af294e1ade493ef9cefa3339d525262f
Cheikh A.B Samb <sambkhadim031@gmail.com>
2025-03-04 12:54:37 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 console.log("Hello, Git !");
-  let ph = 9
+console.log("Here you will see if the ph is Basic , Acidic or Neutral");
+// I just added a line to explain you the utility of this code 
+  let ph = 9 // You just have to change the ph 
   
   if (ph > 7){
     console.log("Basic")
